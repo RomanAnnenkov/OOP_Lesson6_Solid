74fc9e96cccb0d725c7c7a775f1416ae8808c83f
Roman Annenkov <roman.annenkov@gmail.com>
2023-06-12 21:31:45 +0300

    added toString to oredrBonus

--- Solid5Lsp2/src/solid/lsp/OrderBonus.java
+++ Solid5Lsp2/src/solid/lsp/OrderBonus.java
@@ -10,4 +10,9 @@ public class OrderBonus extends OrderAbstract {
     public int getAmount() {
         return 0;
     }
+
+    @Override
+    public String toString() {
+        return String.format("Bonus: %s", super.toString());
+    }
 }
