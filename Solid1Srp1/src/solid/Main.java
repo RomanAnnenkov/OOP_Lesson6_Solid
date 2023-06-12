74fe096c119d48fc4cf6af7fd9d3c853ba48dc0f
Roman Annenkov <roman.annenkov@gmail.com>
2023-06-12 21:01:08 +0300

    added first and second task

--- Solid1Srp1/src/solid/Main.java
+++ Solid1Srp1/src/solid/Main.java
@@ -2,11 +2,19 @@ package solid;
 
 import solid.srp.Point;
 import solid.srp.Square;
+import solid.srp.SquareDraw;
 
 public class Main {
     public static void main(String[] args) {
-        Square square = new Square(new Point(1,1), 5);
+        int side = 5;
+        Square square = new Square(new Point(1,1), side);
+        SquareDraw squareDraw = new SquareDraw(side);
+        squareDraw.draw();
         System.out.printf("Площадь фигуры: %d \n", square.getArea());
-        square.draw();
+
+        squareDraw.setSide(square.getSide() * 2);
+        squareDraw.draw();
+        System.out.printf("Площадь фигуры: %d \n", square.getArea());
+
     }
 }
--- Solid1Srp1/src/solid/srp/Square.java
+++ Solid1Srp1/src/solid/srp/Square.java
@@ -30,20 +30,11 @@ public class Square {
         return side * side;
     }
 
-    public void draw() {
-        for (int i = 0; i < side; i++) {
-            System.out.print("* ");
-        }
-        System.out.println();
-        for (int i = 0; i < side-2; i++) {
-            System.out.print("* ");
-            for (int j = 1; j < side - 1; j++) {
-                System.out.print("  ");
-            }
-            System.out.println("*");
-        }
-        for (int i = 0; i < side; i++) {
-            System.out.print("* ");
-        }
+    public void setSide(int side) {
+        this.side = side;
+    }
+
+    public int getSide() {
+        return side;
     }
 }
